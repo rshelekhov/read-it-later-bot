6cadc67c7a8e9f03ed9c2616d0a16349a1be5799
Roman Shelekhov <shelekhoff@gmail.com>
2024-09-18 10:09:54 +0400

    added event processor

--- events/telegram/messages.go
+++ events/telegram/messages.go
@@ -1,1 +1,17 @@
 package telegram
+
+const msgHelp = `I can save and keep your pages. Also I can offer you them to read.
+
+In order to save the page, just send me a link to it.
+
+In order to get a random page from your list, just send me /rnd command.
+Caution! After that, this page will be removed from your list!`
+
+const msgHello = "Hello! \n\n" + msgHelp
+
+const (
+	msgUnknownCommand = "Unknown command"
+	msgNoSavedPages   = "You have no saved pages"
+	msgSaved          = "Saved"
+	msgAlreadyExists  = "You have already saved this page"
+)
